cc6edffa3af8bfc92493e6afc4e1c8fcb95daf53
sumeet-cybermed <sumeet@cybermedsolutions.com>
2019-11-26 14:24:27 +0530

    all assets moved to scrips_coew for acvcess in scrips_core and sharing app_assets.dart created

---
+++ scrips_core/lib/ui_helpers/app_assets.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Fonts {
+  static String roboto = "Roboto";
+}
+
+class PmImages {
+  static Image back =
+      Image(image: AssetImage("assets/pratice-management/back.png"));
+  static Image blank =
+      Image(image: AssetImage("assets/pratice-management/blank.png"));
+  static Image cameraIcon =
+      Image(image: AssetImage("assets/pratice-management/camera_icon.png"));
+  static Image dashboard =
+      Image(image: AssetImage("assets/pratice-management/dashboard.png"));
+  static Image dropDownIcon =
+      Image(image: AssetImage("assets/pratice-management/drop_down_icon.png"));
+  static Image emailIcon =
+      Image(image: AssetImage("assets/pratice-management/email_icon.png"));
+  static Image greenCheckMark = Image(
+      image: AssetImage("assets/pratice-management/green_check_mark.png"));
+  static Image licenseExpirationDate = Image(
+      image:
+          AssetImage("assets/pratice-management/license_expiration_date.png"));
+  static Image location =
+      Image(image: AssetImage("assets/pratice-management/location.png"));
+  static Image patients =
+      Image(image: AssetImage("assets/pratice-management/patients.png"));
+  static Image pmAppIcon =
+      Image(image: AssetImage("assets/pratice-management/pm_app_icon.png"));
+  static Image reports =
+      Image(image: AssetImage("assets/pratice-management/reports.png"));
+  static Image scripsLogo =
+      Image(image: AssetImage("assets/pratice-management/scrips_logo.png"));
+  static Image settings =
+      Image(image: AssetImage("assets/pratice-management/settings.png"));
+  static Image success =
+      Image(image: AssetImage("assets/pratice-management/success.png"));
+  static Image userIcon =
+      Image(image: AssetImage("assets/pratice-management/user_icon.png"));
+}
