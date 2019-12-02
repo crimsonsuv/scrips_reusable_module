6e89d955542b217663cb23a91b7255f28cc321db
Nilesh <nileshpatel2005@gmail.com>
2019-12-02 11:32:08 +0530

    - Added localisation of string - Updated style at common place - Added scroll view to it.

--- scrips_core/lib/ui_helpers/text_styles.dart
+++ scrips_core/lib/ui_helpers/text_styles.dart
@@ -18,86 +18,67 @@ TextStyle defaultSubHeaderStyle(color, backgroundColor) => TextStyle(
       color: color ?? defaultSubHeaderTextColor,
       backgroundColor: backgroundColor ?? defaultSubHeaderBackgroundColor,
     );
-//
-TextStyle defaultLabelStyle(color, backgroundColor,
-        {bool isReversed = false}) =>
-    TextStyle(
-      fontSize: labelTextSize,
-      fontWeight: FontWeight.w500,
-      color: color ??
-          (!isReversed ? defaultLabelTextColor : defaultReversedLabelTextColor),
-      backgroundColor: backgroundColor ??
-          (!isReversed
-              ? defaultLabelBackgroundColor
-              : defaultReversedLabelBackgroundColor),
-    );
-TextStyle defaultLabelStyleSmall(color, backgroundColor,
-        {bool isReversed = false}) =>
-    TextStyle(
-      fontSize: labelSmallTextSize,
-      fontWeight: FontWeight.w500,
-      color: color ??
-          (!isReversed ? defaultLabelTextColor : defaultReversedLabelTextColor),
-      backgroundColor: backgroundColor ??
-          (!isReversed
-              ? defaultLabelBackgroundColor
-              : defaultReversedLabelBackgroundColor),
-    );
-TextStyle defaultLabelStyleLarge(color, backgroundColor,
-        {bool isReversed = false}) =>
-    TextStyle(
-      fontSize: labelLargeTextSize,
-      fontWeight: FontWeight.w500,
-      color: color ??
-          (!isReversed ? defaultLabelTextColor : defaultReversedLabelTextColor),
-      backgroundColor: backgroundColor ??
-          (!isReversed
-              ? defaultLabelBackgroundColor
-              : defaultReversedLabelBackgroundColor),
-    );
-TextStyle defaultMenuItemTextStyle(color, backgroundColor,
-        {bool isReversed = false}) =>
-    TextStyle(
-      fontSize: menuItemTextSize,
-      fontWeight: FontWeight.w500,
-      color: color ??
-          (!isReversed ? defaultLabelTextColor : defaultReversedLabelTextColor),
-      backgroundColor: backgroundColor ??
-          (!isReversed
-              ? defaultLabelBackgroundColor
-              : defaultReversedLabelBackgroundColor),
-    );
-//
-TextStyle defaultValidationStyle(color, backgroundColor,
-        {bool isReversed = false}) =>
-    TextStyle(
-      fontSize: validationTextSize,
-      fontWeight: FontWeight.w500,
-      color: color ??
-          (!isReversed
-              ? defaultValidationTextColor
-              : defaultReversedValidationTextColor),
-      backgroundColor: backgroundColor ??
-          (!isReversed
-              ? defaultValidationBackgroundColor
-              : defaultReversedValidationBackgroundColor),
-    );
-//
-TextStyle defaultFieldStyle(color, backgroundColor,
-        {bool isReversed = false}) =>
-    TextStyle(
-      fontSize: fieldTextSize,
-      fontWeight: FontWeight.w500,
-      color: color ??
-          (!isReversed ? defaultFieldTextColor : defaultReversedFieldTextColor),
-      backgroundColor: backgroundColor ??
-          (!isReversed
-              ? defaultFieldBackgroundColor
-              : defaultReversedFieldBackgroundColor),
+TextStyle boldLabelTextStyle(size, color) => TextStyle(
+      fontSize: size,
+      fontWeight: FontWeight.bold,
+      color: color ?? textInputColor,
     );
 
-TextStyle defaultHintStyle(color, backgroundColor, {bool isReversed = false}) =>
-    TextStyle(
+TextStyle normalLabelTextStyle(size, color) => TextStyle(
+      fontSize: size,
+      fontWeight: FontWeight.normal,
+      color: color ?? textInputColor,
+    );
+
+// TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold, color: labelTextStyleTextColor)
+TextStyle defaultFieldLabelStyle(color, backgroundColor, {bool isReversed = false}) => TextStyle(
+      fontSize: labelTextSize,
+      fontWeight: FontWeight.bold,
+      color: color ?? (!isReversed ? labelTextStyleTextColor : defaultReversedLabelTextColor),
+      backgroundColor: backgroundColor ?? (!isReversed ? defaultLabelBackgroundColor : defaultReversedLabelBackgroundColor),
+    );
+
+TextStyle defaultLabelStyle(color, backgroundColor, {bool isReversed = false}) => TextStyle(
+      fontSize: labelTextSize,
+      fontWeight: FontWeight.w500,
+      color: color ?? (!isReversed ? defaultLabelTextColor : defaultReversedLabelTextColor),
+      backgroundColor: backgroundColor ?? (!isReversed ? defaultLabelBackgroundColor : defaultReversedLabelBackgroundColor),
+    );
+TextStyle defaultLabelStyleSmall(color, backgroundColor, {bool isReversed = false}) => TextStyle(
+      fontSize: labelSmallTextSize,
+      fontWeight: FontWeight.w500,
+      color: color ?? (!isReversed ? defaultLabelTextColor : defaultReversedLabelTextColor),
+      backgroundColor: backgroundColor ?? (!isReversed ? defaultLabelBackgroundColor : defaultReversedLabelBackgroundColor),
+    );
+TextStyle defaultLabelStyleLarge(color, backgroundColor, {bool isReversed = false}) => TextStyle(
+      fontSize: labelLargeTextSize,
+      fontWeight: FontWeight.w500,
+      color: color ?? (!isReversed ? defaultLabelTextColor : defaultReversedLabelTextColor),
+      backgroundColor: backgroundColor ?? (!isReversed ? defaultLabelBackgroundColor : defaultReversedLabelBackgroundColor),
+    );
+TextStyle defaultMenuItemTextStyle(color, backgroundColor, {bool isReversed = false}) => TextStyle(
+      fontSize: menuItemTextSize,
+      fontWeight: FontWeight.w500,
+      color: color ?? (!isReversed ? defaultLabelTextColor : defaultReversedLabelTextColor),
+      backgroundColor: backgroundColor ?? (!isReversed ? defaultLabelBackgroundColor : defaultReversedLabelBackgroundColor),
+    );
+//
+TextStyle defaultValidationStyle(color, backgroundColor, {bool isReversed = false}) => TextStyle(
+      fontSize: validationTextSize,
+      fontWeight: FontWeight.w500,
+      color: color ?? (!isReversed ? defaultValidationTextColor : defaultReversedValidationTextColor),
+      backgroundColor:
+          backgroundColor ?? (!isReversed ? defaultValidationBackgroundColor : defaultReversedValidationBackgroundColor),
+    );
+//
+TextStyle defaultFieldStyle(color, backgroundColor, {bool isReversed = false}) => TextStyle(
+      fontSize: fieldTextSize,
+      fontWeight: FontWeight.w500,
+      color: color ?? (!isReversed ? defaultFieldTextColor : defaultReversedFieldTextColor),
+      backgroundColor: backgroundColor ?? (!isReversed ? defaultFieldBackgroundColor : defaultReversedFieldBackgroundColor),
+    );
+
+TextStyle defaultHintStyle(color, backgroundColor, {bool isReversed = false}) => TextStyle(
       fontSize: hintTextSize,
       fontWeight: FontWeight.w500,
       color: defaultFieldHintColor,
