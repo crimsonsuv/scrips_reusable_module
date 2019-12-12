837757f88ff150ee32f128bca677004a475c406f
Nilesh <nileshpatel2005@gmail.com>
2019-12-12 12:33:18 +0530

    - Updated UI of Search Bar on List & Added Last Sign In (WIP)

--- scrips_core/lib/ui_helpers/app_colors.dart
+++ scrips_core/lib/ui_helpers/app_colors.dart
@@ -18,7 +18,7 @@ final Color textFieldBGcolor = Color(0xFFF4F7F8); //getColorFromHex('#F4F7F8');
 final Color labelTextStyleTextColor = Color(0xFFAAAFC0); //getColorFromHex('#AAAFC0');
 
 final Color regularTextColor = Color(0xFF485685);
-final Color searchBGColour = Color(0xFF8E8E93).withAlpha(88); //
+final Color searchBGColour = Color(0xFF8E8E93).withOpacity(0.12); //
 
 // mainMenuBackground
 const Color defaultMainMenuBackgroundColor = Color(0xFF1F3888);
