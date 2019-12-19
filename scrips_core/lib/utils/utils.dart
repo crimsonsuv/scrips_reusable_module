1ed92d294879226f585b2c2b22f05c39f48574ee
Radivoje Ostojic <radivojeostojic@gmail.com>
2019-12-19 18:16:23 +0100

    Organization list update

--- scrips_core/lib/utils/utils.dart
+++ scrips_core/lib/utils/utils.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
 
 Color getColorFromHex(String hexColor) {
   hexColor = hexColor.toUpperCase().replaceAll('#', '');
@@ -20,3 +21,12 @@ String _debugLogPrefix = 'SCRIPS';
 void debugLog(String message) {
   debugPrint('$_debugLogPrefix - ${DateTime.now().toString()} - $message');
 }
+
+String scDateFormat(val){
+  if(val == null){
+    return "";
+  } else {
+    return DateFormat('dd-MM-yyyy')?.format(val);
+  }
+}
+
