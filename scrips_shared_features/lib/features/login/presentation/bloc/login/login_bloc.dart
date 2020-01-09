73de2358da11c8af8b16384a0a813533fd49db1c
Suvankar <suv.das19@gmail.com>
2020-01-10 01:20:20 +0530

    MSP1-484 Side Menu and Sub Menu UI Complete with Animation

--- scrips_shared_features/lib/features/login/presentation/bloc/login/login_bloc.dart
+++ scrips_shared_features/lib/features/login/presentation/bloc/login/login_bloc.dart
@@ -6,11 +6,9 @@ import 'package:meta/meta.dart';
 import 'package:scrips_core/data_models/login/login.dart';
 import 'package:scrips_core/data_models/user/user.dart';
 import 'package:scrips_core/general/property_info.dart';
-import 'package:scrips_shared_features/core/constants/app_config.dart';
 import 'package:scrips_shared_features/features/login/domain/usecase/get_login_response_use_case.dart';
 
 part 'login_event.dart';
-
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
@@ -26,10 +24,12 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
 
   @override
   Stream<LoginState> mapEventToState(LoginEvent event) async* {
+    String dummyUserName = "user1@scrips.com";
+    String dummyUserPassword = "123456";
     if (event is SetLoginDummyDataEvent) {
       User user = User(
-          email: PropertyInfo(Configuration.dummyUserName),
-          password: PropertyInfo(Configuration.dummyUserPassword));
+          email: PropertyInfo(dummyUserName),
+          password: PropertyInfo(dummyUserPassword));
       yield LoginDummyDataState(user);
     } else if (event is GetLoginResponseEvent) {
       yield LoginLoading(true);
