79d58787506a7ce73afa9da7badc4bc2911d5f86
Suvankar Das <terri@realviews.com.au>
2020-04-10 16:29:44 +0530

    Practice test api set

--- scrips_shared_features/lib/core/constants/api_constats.dart
+++ scrips_shared_features/lib/core/constants/api_constats.dart
@@ -1,4 +1,5 @@
-const String patientsTestServer = "https://scripspatients.azurewebsites.net";
+const String patientsTestServer = "http://52.25.96.244:7017";
 const String identityTestServer = "http://identitystaging.azurewebsites.net";
 const String identityLiveServer =
     "https://scripsidentityapi20191030115107.azurewebsites.net";
+const String patientsLiveServer = "https://scripspatients.azurewebsites.net";
--- scrips_shared_features/lib/features/login/data/datasources/login_data_source_impl.dart
+++ scrips_shared_features/lib/features/login/data/datasources/login_data_source_impl.dart
@@ -22,8 +22,8 @@ class LoginDataSourceImpl extends LoginDataSource {
 
   @override
   Future<LoginUserData> oauth2Login() async {
-    final url = Uri.https('scripsidentityapi20191030115107.azurewebsites.net',
-        '/connect/authorize', {
+    final url =
+        Uri.https('identitystaging.azurewebsites.net', '/connect/authorize', {
       'response_type': 'code',
       'grant_type': 'authorization_code',
       'client_id': 'Scrips.Consumer',
