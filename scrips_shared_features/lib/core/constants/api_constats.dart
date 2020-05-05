01da47c7d05cd237dbd31ee958177b3d4a9efe58
Suvankar <suv.das19@gmail.com>
2020-05-05 15:39:01 +0530

    MSP1-923 Dynamic image implementation

--- scrips_shared_features/lib/core/constants/api_constats.dart
+++ scrips_shared_features/lib/core/constants/api_constats.dart
@@ -1,9 +1,9 @@
-//const String patientsServer = "http://52.25.96.244:7017";
-const String patientsServer = "https://scripspatients.azurewebsites.net";
+const String patientsServer = "http://52.25.96.244:7017";
+//const String patientsServer = "https://scripspatients.azurewebsites.net";
 
-//const String identityServer = "http://identitystaging.azurewebsites.net";
-const String identityServer =
-    "https://scripsidentityapi20191030115107.azurewebsites.net";
+const String identityServer = "http://identitystaging.azurewebsites.net";
+//const String identityServer =
+//    "https://scripsidentityapi20191030115107.azurewebsites.net";
 
-const String practiceServer = "http://scripspracticesetup.azurewebsites.net";
-//const String practiceServer = "http://52.25.96.244:7008";
+//const String practiceServer = "http://scripspracticesetup.azurewebsites.net";
+const String practiceServer = "http://52.25.96.244:7008";
