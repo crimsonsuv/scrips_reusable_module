70c0debd37c9100a73f84dc132545e88ddcc7be8
Suvankar Das <suv.das19@gmail.com>
2020-06-05 17:35:35 +0530

    MSP1-1029, 1030 Doctor and Staff APi Integration complete

--- scrips_shared_features/lib/core/constants/api_constats.dart
+++ scrips_shared_features/lib/core/constants/api_constats.dart
@@ -11,3 +11,6 @@ const String practiceServer = "http://52.25.96.244:7008";
 const String oath2LoginUrl = "identitystaging.azurewebsites.net";
 //const String oath2LoginUrl =
 //    "scripsidentityapi20191030115107.azurewebsites.net";
+
+const String providerServer = "http://52.25.96.244:7023";
+//const String providerServer = "https://scripsprovidersetup.azurewebsites.net";
